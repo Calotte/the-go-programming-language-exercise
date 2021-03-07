8b43b8514a0d4f1881559a29399ae0895dcf7cf2
chashi <chashi@microsoft.com>
2021-03-07 09:16:44 +0800

    add

---
+++ ch8/crawl1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"the-go-programming-language-exercise/ch5/links"
+)
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	var n int
+	n++
+	// Start with the command-line arguments.
+	go func() { worklist <- os.Args[1:] }()
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
